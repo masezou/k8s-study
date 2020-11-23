d31b01511b3423e116a7c753ac40ddeb3fb77e01
Masahiro Takahashi <masezou@gmail.com>
2020-11-23 10:29:50 +0000

    Fun Game bonus

---
+++ kube-doom.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/storax/kubedoom
+cd kubedoom
+kubectl apply -f manifest/
+namespace/kubedoom created
+deployment.apps/kubedoom created
+serviceaccount/kubedoom created
+clusterrolebinding.rbac.authorization.k8s.io/kubedoom created
+
+echo ""
+echo "*************************************************************************************"
+echo "Next Step"
+echo "kubectl -n kubedoom port-forward --address 0.0.0.0 deployment/kubedoom 5900:5900"
+echo "connect hostIP:5900 with vncviewer"
+echo "Password is idbehold"
