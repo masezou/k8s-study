012c83f54ea2aa21cdd057ce5ef655b76c147e1d
Masahiro Takahashi <masezou@gmail.com>
2020-12-01 03:57:27 +0000

    Pull from master2

--- 1-buildk8s.sh
+++ 1-buildk8s.sh
@@ -9,7 +9,7 @@ fi
 
 # Install Kind
 if [ ! -f /usr/local/bin/kind ]; then
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+curl -s -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 kind completion bash > /etc/bash_completion.d/kind
@@ -18,13 +18,12 @@ fi
 
 # Install kubectl
 if [ ! -f /usr/bin/kubectl ]; then
+apt update
 apt -y install docker.io apt-transport-https gnupg2 curl
 systemctl enable --now docker
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-apt update
 apt -y install kubectl
-apt clean
 kubectl completion bash >/etc/bash_completion.d/kubectl
 source /etc/bash_completion.d/kubectl
 echo 'export KUBE_EDITOR=vi' >>~/.bashrc
@@ -32,7 +31,7 @@ fi
 
 # Install Helm
 if [ ! -f /usr/local/bin/helm ]; then
-curl -O https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+curl -s -O https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 bash ./get-helm-3
 helm version
 rm get-helm-3
@@ -41,9 +40,14 @@ source /etc/bash_completion.d/helm
 fi
 
 # Bulding Kind Cluster
-#kind create cluster --name k10-demo --image kindest/node:v1.18.2 --wait 600s
-kind create cluster --config multi-node.yaml --name k10-demo --image kindest/node:v1.18.2 --wait 600s
-kind get kubeconfig --name k10-demo  > ~/kubeconfig.yaml
+kind create cluster --name k10-demo --image kindest/node:v1.18.2 --wait 600s
+#kind create cluster --config multi-node.yaml --name k10-demo --image kindest/node:v1.18.2 --wait 600s
+#kind get kubeconfig --name k10-demo  > ~/kubeconfig-k10-demo.yaml
+#kind create cluster --config multi-node.yaml --name k10-demo-dr --image kindest/node:v1.18.2 --wait 600s
+#kind get kubeconfig --name k10-demo-dr  > ~/kubeconfig-k10-demo-dr.yaml
+
+#kubectl config use-context kind-k10-demo
+kubectl config get-contexts
 
 echo ""
 echo "*************************************************************************************"
