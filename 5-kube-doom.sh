720f6e7001bc7e08cd637cdf026c8c0c646a0724
Masahiro Takahashi <masezou@gmail.com>
2021-05-23 01:41:04 +0000

    fixed typo

--- 5-kube-doom.sh
+++ 5-kube-doom.sh
@@ -1,10 +1,7 @@
 git clone https://github.com/storax/kubedoom
 cd kubedoom
 kubectl apply -f manifest/
-namespace/kubedoom created
-deployment.apps/kubedoom created
-serviceaccount/kubedoom created
-clusterrolebinding.rbac.authorization.k8s.io/kubedoom created
+cd ..
 mv kubedoom kubedoom-`date "+%Y%m%d_%H%M%S"`
 
 echo ""
