5be9389acf59582b9b58ed5b871a433e56242f49
Masahiro Takahashi <masezou@gmail.com>
2021-05-26 12:44:20 +0000

    added helm update

--- 2-storage.sh
+++ 2-storage.sh
@@ -56,6 +56,7 @@ showmount -e
 fi
 
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm repo update
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --set nfs.server=${LOCALIPADDR} \
     --set nfs.path=/nfsexport
