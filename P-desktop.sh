c34558d4830a9b4f29d26bb6aeac85e5e0c66903
Masahiro Takahashi <masezou@gmail.com>
2021-06-23 14:55:08 +0000

    for desktop

---
+++ P-desktop.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#########################################################
+### UID Check ###
+if [ ${EUID:-${UID}} != 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+else
+    echo "I am root user."
+fi
+
+### Distribution Check ###
+lsb_release -d | grep Ubuntu | grep 20.04
+DISTVER=$?
+if [ ${DISTVER} = 1 ]; then
+    echo "only supports Ubuntu 20.04 server"
+    exit 1
+else
+    echo "Ubuntu 20.04=OK"
+fi
+
+apt-get -y install ubuntu-desktop
+init 5
