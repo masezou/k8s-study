7c012e0e845694cda7ee57fc505cfcc508d983bc
Masahiro Takahashi <masezou@gmail.com>
2021-07-20 03:43:44 +0000

    Change to Stateful Set

--- P-wordpress.sh
+++ P-wordpress.sh
@@ -1,33 +1,18 @@
 #!/usr/bin/env bash
-#### LOCALIP #########
-ip address show ens160 >/dev/null
-retval=$?
-if [ ${retval} -eq 0 ]; then
-        LOCALIPADDR=`ip -f inet -o addr show ens160 |cut -d\  -f 7 | cut -d/ -f 1`
-else
-  ip address show ens192 >/dev/null
-  retval2=$?
-  if [ ${retval2} -eq 0 ]; then
-        LOCALIPADDR=`ip -f inet -o addr show ens192 |cut -d\  -f 7 | cut -d/ -f 1`
-  else
-        LOCALIPADDR=`ip -f inet -o addr show eth0 |cut -d\  -f 7 | cut -d/ -f 1`
-  fi
-fi
-echo ${LOCALIPADDR}
-
-
 NAMESPACE=wordpress-hostpath
 
 kubectl create namespace ${NAMESPACE}
 mkdir ${NAMESPACE}
 cd  ${NAMESPACE}
-cat <<EOF>wordpress-pv-claim.yaml
+
+cat << EOF > wordpress-pvc.yaml
 apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:
   name: wp-pv-claim
   labels:
     app: wordpress
+    tier: wordpress
 spec:
   accessModes:
     - ReadWriteOnce
@@ -35,61 +20,7 @@ spec:
     requests:
       storage: 20Gi
 EOF
-cat <<EOF>mysql-pv-claim.yaml     
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: mysql-pv-claim
-  labels:
-    app: wordpress
-spec:
-  accessModes:
-    - ReadWriteOnce
-  resources:
-    requests:
-      storage: 20Gi
-EOF
-kubectl apply -f wordpress-pv-claim.yaml -n ${NAMESPACE}
-kubectl apply -f mysql-pv-claim.yaml -n ${NAMESPACE}
-kubectl get pvc -n ${NAMESPACE}
-cat <<EOF> mysql-deployment.yaml
-apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
-kind: Deployment
-metadata:
-  name: wordpress-mysql
-  labels:
-    app: wordpress
-spec:
-  selector:
-    matchLabels:
-      app: wordpress
-      tier: mysql
-  strategy:
-    type: Recreate
-  template:
-    metadata:
-      labels:
-        app: wordpress
-        tier: mysql
-    spec:
-      containers:
-      - image: mysql:5.6
-        name: mysql
-        env:
-        - name: MYSQL_ROOT_PASSWORD
-          value: password
-        ports:
-        - containerPort: 3306
-          name: mysql
-        volumeMounts:
-        - name: mysql-persistent-storage
-          mountPath: /var/lib/mysql
-      volumes:
-      - name: mysql-persistent-storage
-        persistentVolumeClaim:
-          claimName: mysql-pv-claim
-EOF
-cat <<EOF> wordpress-deployment.yaml
+cat << EOF > wordpress.yaml
 apiVersion: apps/v1 # for versions before 1.9.0 use apps/v1beta2
 kind: Deployment
 metadata:
@@ -114,9 +45,12 @@ spec:
         name: wordpress
         env:
         - name: WORDPRESS_DB_HOST
-          value: wordpress-mysql
+          value: mysql-release
         - name: WORDPRESS_DB_PASSWORD
-          value: password
+          valueFrom:
+              secretKeyRef:
+                name: mysql-release
+                key: mysql-root-password
         ports:
         - containerPort: 80
           name: wordpress
@@ -128,43 +62,37 @@ spec:
         persistentVolumeClaim:
           claimName: wp-pv-claim
 EOF
-kubectl apply -f mysql-deployment.yaml -n ${NAMESPACE}
-kubectl apply -f wordpress-deployment.yaml -n ${NAMESPACE}
-kubectl get pod -n ${NAMESPACE}
-kubectl get deployment -n ${NAMESPACE}
-cat <<EOF> mysql-service.yaml
+cat << EOF > wordpress-service.yaml
 apiVersion: v1
 kind: Service
 metadata:
-  name: wordpress-mysql
   labels:
     app: wordpress
-spec:
-  ports:
-    - port: 3306
-  selector:
-    app: wordpress
-    tier: mysql
-EOF
-cat <<EOF>wordpress-service.yaml
-apiVersion: v1
-kind: Service
-metadata:
   name: wordpress
-  labels:
-    app: wordpress
 spec:
+  type: LoadBalancer
   ports:
     - port: 80
+      targetPort: 80
+      protocol: TCP
   selector:
     app: wordpress
-    tier: frontend
-  type: LoadBalancer
 EOF
-kubectl apply -f mysql-service.yaml -n ${NAMESPACE}
-kubectl apply -f wordpress-service.yaml  -n ${NAMESPACE}
-kubectl get svc -n ${NAMESPACE}
+
+helm install mysql-release bitnami/mysql --set volumePermissions.enabled=true -n ${NAMESPACE}
+while [ "$(kubectl get pod -n ${NAMESPACE} mysql-release-0 --output="jsonpath={.status.containerStatuses[*].ready}" | cut -d' ' -f2)" != "true" ]; do
+	echo "Deploying Stateful MySQL, Please wait...."
+	sleep 30
+done
+kubectl create -f wordpress-pvc.yaml -n ${NAMESPACE}
+kubectl get pvc,pv
+kubectl create -f wordpress.yaml -n ${NAMESPACE}
+kubectl get pod -l app=wordpress -n ${NAMESPACE}
+kubectl create -f wordpress-service.yaml -n ${NAMESPACE}
+kubectl get svc -l app=wordpress -n ${NAMESPACE}
+kubectl get pod -n ${NAMESPACE}
 cd ..
+
 mv ${NAMESPACE} ${NAMESPACE}-`date "+%Y%m%d_%H%M%S"`
 
 echo ""
@@ -173,9 +101,7 @@ echo "Next Step"
 echo "Confirm wordpress pod and mysql pod are running with kubectl get pod -A"
 echo "run kubectl -n ${NAMESPACE} get svc"
 echo "Open your browser http://external ip)"
-echo "You can test access with lynx  http://external ip)" 
+echo "You can test access with lynx  http://external ip)"
 echo "or"
 echo "kubectl port-forward --address 0.0.0.0 svc/wordpress 8081:80 -n wordpress-hostpath"
 echo "http://${LOCALIPADDR}:8081"
-
-
