e03ab27f01d55f901b8b22b0130d68a2b2062425
Masahiro Takahashi <masezou@gmail.com>
2021-07-31 05:41:28 +0000

    rename files

---
+++ K1-kasten.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env bash
+
+#########################################################
+### UID Check ###
+if [ ${EUID:-${UID}} != 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+else
+    echo "I am root user."
+fi
+
+### Distribution Check ###
+lsb_release -d | grep Ubuntu | grep 20.04
+DISTVER=$?
+if [ ${DISTVER} = 1 ]; then
+    echo "only supports Ubuntu 20.04 server"
+    exit 1
+else
+    echo "Ubuntu 20.04=OK"
+fi
+
+### ARCH Check ###
+PARCH=`arch`
+if [ ${PARCH} = aarch64 ]; then
+  ARCH=arm64
+  echo ${ARCH}
+elif [ ${PARCH} = arm64 ]; then
+  ARCH=arm64
+  echo ${ARCH}
+elif [ ${PARCH} = x86_64 ]; then
+  ARCH=amd64
+  echo ${ARCH}
+else
+  echo "${ARCH} platform is not supported"
+  exit 1
+fi
+
+#### LOCALIP #########
+ip address show ens160 >/dev/null
+retval=$?
+if [ ${retval} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens160 |cut -d\  -f 7 | cut -d/ -f 1`
+else
+  ip address show ens192 >/dev/null
+  retval2=$?
+  if [ ${retval2} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens192 |cut -d\  -f 7 | cut -d/ -f 1`
+  else
+        LOCALIPADDR=`ip -f inet -o addr show eth0 |cut -d\  -f 7 | cut -d/ -f 1`
+  fi
+fi
+echo ${LOCALIPADDR}
+
+#########################################################
+
+# htpasswd for basic auth
+apt -y install apache2-utils
+
+# Install K10-tools
+TOOLSVER=4.0.8
+if [ ! -f /usr/local/bin/k10tools ]; then
+curl -OL https://github.com/kastenhq/external-tools/releases/download/${TOOLSVER}/k10tools_${TOOLSVER}_linux_${ARCH}
+mv k10tools_${TOOLSVER}_linux_${ARCH} /usr/local/bin/k10tools
+chmod +x /usr/local/bin/k10tools
+fi
+
+if [ ! -f /usr/local/bin/k10multicluster ]; then
+curl -OL https://github.com/kastenhq/external-tools/releases/download/${TOOLSVER}/k10multicluster_${TOOLSVER}_linux_${ARCH}
+mv k10multicluster_${TOOLSVER}_linux_${ARCH}  /usr/local/bin/k10multicluster
+chmod +x /usr/local/bin/k10multicluster
+fi
+
+if [ ${ARCH} = "amd64" ]; then
+KUBESTRVER=0.4.17
+if [ ! -f /usr/local/bin/kubestr ]; then
+mkdir temp
+cd temp
+curl -OL https://github.com/kastenhq/kubestr/releases/download/v${KUBESTRVER}/kubestr-v${KUBESTRVER}-linux-${ARCH}.tar.gz
+tar xfz kubestr-v${KUBESTRVER}-linux-${ARCH}.tar.gz
+rm kubestr-v${KUBESTRVER}-linux-${ARCH}.tar.gz
+mv kubestr /usr/local/bin/kubestr
+chmod +x /usr/local/bin/kubestr
+cd ..
+rm -rf temp
+fi
+
+# Pre-req Kasten
+helm repo add kasten https://charts.kasten.io/
+helm repo update
+
+kubectl get volumesnapshotclass | grep csi-hostpath-snapclass
+retval2=$?
+if [ ${retval2} -eq 0 ]; then
+kubectl annotate volumesnapshotclass csi-hostpath-snapclass \
+    k10.kasten.io/is-snapshot-class=true
+fi
+
+kubectl get volumesnapshotclass | grep csi-rbdplugin-snapclass
+retval3=$?
+if [ ${retval3} -eq 0 ]; then
+kubectl annotate volumesnapshotclass csi-rbdplugin-snapclass \
+    k10.kasten.io/is-snapshot-class=true
+fi
+
+curl https://docs.kasten.io/tools/k10_primer.sh | bash
+rm k10primer.yaml
+
+# Install Kasten
+kubectl create namespace kasten-io
+helm install k10 kasten/k10 --namespace=kasten-io \
+--set services.securityContext.runAsUser=0 \
+--set services.securityContext.fsGroup=0 \
+--set prometheus.server.securityContext.runAsUser=0 \
+--set prometheus.server.securityContext.runAsGroup=0 \
+--set prometheus.server.securityContext.runAsNonRoot=false \
+--set prometheus.server.securityContext.fsGroup=0 \
+--set global.persistence.size=40G \
+--set global.persistence.storageClass=csi-hostpath-sc \
+--set injectKanisterSidecar.enabled=true \
+--set auth.tokenAuth.enabled=true \
+--set externalGateway.create=true \
+--set ingress.create=true
+
+# define NFS storage
+kubectl get csidrivers.storage.k8s.io | grep nfs
+retval4=$?
+if [ ${retval4} -eq 0 ]; then
+cat <<EOF | kubectl apply -n kasten-io -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+   name: kastenbackup-pvc
+spec:
+   storageClassName: nfs-csi
+   accessModes:
+      - ReadWriteMany
+   resources:
+      requests:
+         storage: 20Gi
+EOF
+fi
+
+echo "Following is login token"
+sa_secret=$(kubectl get serviceaccount k10-k10 -o jsonpath="{.secrets[0].name}" --namespace kasten-io)
+kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\n'}" | base64 --decode > k10-k10.token
+echo "" >> k10-k10.token
+cat k10-k10.token
+echo
+
+EXTERNALIP=`kubectl -n kasten-io get ingress | awk '{print $4}' | tail -n 1`
+
+echo ""
+echo "*************************************************************************************"
+echo "Next Step"
+echo "Confirm kasten is running with kubectl get pods --namespace kasten-io"
+echo "kubectl --namespace kasten-io port-forward --address 0.0.0.0 service/gateway 8080:8000"
+echo "Open your browser http://${LOCALIPADDR}:8080/k10/#/"
+echo "or"
+echo "Open http://${EXTERNALIP}/10 from local browser"
+echo "then input login token"
+echo "Note:"
+echo 
+
+else
+	echo "kubestr: ${ARCH} is not supported"
+	echo "Kasten-io: ${ARCH} is not supported"
+fi
---
+++ K2-kasten-setup.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+#### LOCALIP #########
+ip address show ens160 >/dev/null
+retval=$?
+if [ ${retval} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens160 |cut -d\  -f 7 | cut -d/ -f 1`
+else
+  ip address show ens192 >/dev/null
+  retval2=$?
+  if [ ${retval2} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens192 |cut -d\  -f 7 | cut -d/ -f 1`
+  else
+        LOCALIPADDR=`ip -f inet -o addr show eth0 |cut -d\  -f 7 | cut -d/ -f 1`
+  fi
+fi
+echo ${LOCALIPADDR}
+
+# Configure default users
+#Backup Admin
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: backupadmin
+  namespace: default
+EOF
+sa_secret=$(kubectl get serviceaccount backupadmin -o jsonpath="{.secrets[0].name}")
+kubectl get secret $sa_secret  -ojsonpath="{.data.token}{'\n'}" | base64 --decode > backupadmin.token
+echo "" >> backupadmin.token
+kubectl get serviceaccounts
+kubectl get serviceaccounts backupadmin -o yaml
+
+kubectl create clusterrolebinding backupadmin-rolebinding --clusterrole=k10-admin  --serviceaccount=default:backupadmin
+
+#Backup Basic
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: backupbasic
+  namespace: default
+EOF
+sa_secret=$(kubectl get serviceaccount backupbasic -o jsonpath="{.secrets[0].name}")
+kubectl get secret $sa_secret  -ojsonpath="{.data.token}{'\n'}" | base64 --decode > backupbasic.token
+echo "" >> backupbasic.token
+kubectl get serviceaccounts
+kubectl get serviceaccounts backupbasic -o yaml
+
+kubectl create clusterrolebinding backupbasic-rolebinding --clusterrole=k10-basic  --serviceaccount=default:backupbasic
+
+#Backup View
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: backupview
+  namespace: default
+EOF
+sa_secret=$(kubectl get serviceaccount backupview -o jsonpath="{.secrets[0].name}")
+kubectl get secret $sa_secret  -ojsonpath="{.data.token}{'\n'}" | base64 --decode > backupview.token
+echo "" >> backupview.token
+kubectl get serviceaccounts
+kubectl get serviceaccounts backupview -o yaml
+
+kubectl create clusterrolebinding backupview-rolebinding --clusterrole=k10-config-view  --serviceaccount=default:backupview
+
+
+# Configure local minio
+AWS_ACCESS_KEY_ID=` echo -n "minioadminuser" | base64`
+AWS_SECRET_ACCESS_KEY_ID=` echo -n "minioadminuser" | base64`
+
+cat << EOF | kubectl -n kasten-io create -f -
+apiVersion: v1
+data:
+  aws_access_key_id: ${AWS_ACCESS_KEY_ID}
+  aws_secret_access_key: ${AWS_SECRET_ACCESS_KEY_ID}
+kind: Secret
+metadata:
+  name: k10-s3-secret
+  namespace: kasten-io
+type: secrets.kanister.io/aws
+EOF
+cat << EOF | kubectl -n kasten-io create -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Profile
+metadata:
+  name: minio-profile
+  namespace: kasten-io
+spec:
+  type: Location
+  locationSpec:
+    credential:
+      secretType: AwsAccessKey
+      secret:
+        apiVersion: v1
+        kind: Secret
+        name: k10-s3-secret
+        namespace: kasten-io
+    type: ObjectStore
+    objectStore:
+      name: miniobucket
+      objectStoreType: S3
+      endpoint: 'https://${LOCALIPADDR}:9000'
+      skipSSLVerify: true
+      region: us-east-1
+EOF
+sleep 5
+kubectl -n kasten-io get profiles.config.kio.kasten.io
+
