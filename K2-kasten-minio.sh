2516e8fa480d579865d3603420187da0ac69dc50
Masahiro Takahashi <masezou@gmail.com>
2021-08-02 21:42:06 +0000

    devided setup script

---
+++ K2-kasten-minio.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+#### LOCALIP #########
+ip address show ens160 >/dev/null
+retval=$?
+if [ ${retval} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens160 |cut -d\  -f 7 | cut -d/ -f 1`
+else
+  ip address show ens192 >/dev/null
+  retval2=$?
+  if [ ${retval2} -eq 0 ]; then
+        LOCALIPADDR=`ip -f inet -o addr show ens192 |cut -d\  -f 7 | cut -d/ -f 1`
+  else
+        LOCALIPADDR=`ip -f inet -o addr show eth0 |cut -d\  -f 7 | cut -d/ -f 1`
+  fi
+fi
+echo ${LOCALIPADDR}
+# Configure local minio
+
+
+
+AWS_ACCESS_KEY_ID=` echo -n "minioadminuser" | base64`
+AWS_SECRET_ACCESS_KEY_ID=` echo -n "minioadminuser" | base64`
+
+cat << EOF | kubectl -n kasten-io create -f -
+apiVersion: v1
+data:
+  aws_access_key_id: ${AWS_ACCESS_KEY_ID}
+  aws_secret_access_key: ${AWS_SECRET_ACCESS_KEY_ID}
+kind: Secret
+metadata:
+  name: k10-s3-secret
+  namespace: kasten-io
+type: secrets.kanister.io/aws
+EOF
+cat << EOF | kubectl -n kasten-io create -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Profile
+metadata:
+  name: minio-profile
+  namespace: kasten-io
+spec:
+  type: Location
+  locationSpec:
+    credential:
+      secretType: AwsAccessKey
+      secret:
+        apiVersion: v1
+        kind: Secret
+        name: k10-s3-secret
+        namespace: kasten-io
+    type: ObjectStore
+    objectStore:
+      name: miniobucket
+      objectStoreType: S3
+      endpoint: 'https://${LOCALIPADDR}:9000'
+      skipSSLVerify: true
+      region: us-east-1
+EOF
+sleep 5
+kubectl -n kasten-io get profiles.config.kio.kasten.io
+
