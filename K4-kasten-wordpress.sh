b0dad595d8877c8f6622e99617f3eb8577f1e211
Masahiro Takahashi <masezou@gmail.com>
2021-08-03 08:46:15 +0000

    use master

--- K4-kasten-wordpress.sh
+++ K4-kasten-wordpress.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 kubectl --namespace kasten-io apply -f \
-    https://raw.githubusercontent.com/kanisterio/kanister/0.63.0/examples/stable/mysql/blueprint-v2/mysql-blueprint.yaml
+#    https://raw.githubusercontent.com/kanisterio/kanister/0.65.0/examples/stable/mysql/blueprint-v2/mysql-blueprint.yaml
+     https://raw.githubusercontent.com/kanisterio/kanister/master/examples/stable/mysql/blueprint-v2/mysql-blueprint.yaml
 
 #kubectl --namespace mysql annotate statefulset/mysql-release \
 #    kanister.kasten.io/blueprint=mysql-blueprint
--- P-postgresql-lb.sh
+++ P-postgresql-lb.sh
@@ -5,6 +5,7 @@ helm repo update
 kubectl create namespace ${PGNAMESPACE}
 helm install --namespace ${PGNAMESPACE} postgres bitnami/postgresql --version 9.1.1 --set volumePermissions.enabled=true
 kubectl --namespace kasten-io apply -f \
-    https://raw.githubusercontent.com/kanisterio/kanister/0.63.0/examples/stable/postgresql/blueprint-v2/postgres-blueprint.yaml
+#    https://raw.githubusercontent.com/kanisterio/kanister/0.65.0/examples/stable/postgresql/blueprint-v2/postgres-blueprint.yaml
+     https://raw.githubusercontent.com/kanisterio/kanister/master/examples/stable/postgresql/blueprint-v2/postgres-blueprint.yaml
 kubectl --namespace ${PGNAMESPACE} annotate statefulset/postgres-postgresql \
     kanister.kasten.io/blueprint=postgres-bp
