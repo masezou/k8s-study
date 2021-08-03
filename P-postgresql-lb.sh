3eb5ec2068738e08b2e75d9b4bd331f29b72a2b5
Masahiro Takahashi <masezou@gmail.com>
2021-08-03 04:55:30 +0000

    fix typo

--- P-postgresql-lb.sh
+++ P-postgresql-lb.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 PGNAMESPACE=postgresql-lb
-
+helm repo update
 kubectl create namespace ${PGNAMESPACE}
-helm install --namespace ${PGNAMESPACE} postgres bitnami/postgresqli --version 9.1.1 --set volumePermissions.enabled=true
+helm install --namespace ${PGNAMESPACE} postgres bitnami/postgresql --version 9.1.1 --set volumePermissions.enabled=true
 kubectl --namespace kasten-io apply -f \
     https://raw.githubusercontent.com/kanisterio/kanister/0.63.0/examples/stable/postgresql/blueprint-v2/postgres-blueprint.yaml
 kubectl --namespace ${PGNAMESPACE} annotate statefulset/postgres-postgresql \
