9a3d9f21f0a67b72a4d67ef7082dde42946a5445
Masahiro Takahashi <masezou@gmail.com>
2021-08-03 04:52:59 +0000

    correct versions

--- P-postgresql-lb.sh
+++ P-postgresql-lb.sh
@@ -3,7 +3,7 @@
 PGNAMESPACE=postgresql-lb
 
 kubectl create namespace ${PGNAMESPACE}
-helm install --namespace ${PGNAMESPACE} postgres bitnami/postgresqli --version 9.0.0 --set volumePermissions.enabled=true
+helm install --namespace ${PGNAMESPACE} postgres bitnami/postgresqli --version 9.1.1 --set volumePermissions.enabled=true
 kubectl --namespace kasten-io apply -f \
     https://raw.githubusercontent.com/kanisterio/kanister/0.63.0/examples/stable/postgresql/blueprint-v2/postgres-blueprint.yaml
 kubectl --namespace ${PGNAMESPACE} annotate statefulset/postgres-postgresql \
