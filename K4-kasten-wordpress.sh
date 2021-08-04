9adba9d0f4902009df206b9553847b31921f0855
Masahiro Takahashi <masezou@gmail.com>
2021-08-04 00:11:38 +0000

    small fix

--- K4-kasten-wordpress.sh
+++ K4-kasten-wordpress.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl --namespace kasten-io apply -f \
 #    https://raw.githubusercontent.com/kanisterio/kanister/0.65.0/examples/stable/mysql/blueprint-v2/mysql-blueprint.yaml
+kubectl --namespace kasten-io apply -f \
      https://raw.githubusercontent.com/kanisterio/kanister/master/examples/stable/mysql/blueprint-v2/mysql-blueprint.yaml
 
 #kubectl --namespace mysql annotate statefulset/mysql-release \
