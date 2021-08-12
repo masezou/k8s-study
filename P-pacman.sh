4ec910bfac65d01d2943b61d28ff0af77e4e4d46
Masahiro Takahashi <masezou@gmail.com>
2021-08-12 04:27:48 +0000

    clone only latest version

--- P-pacman.sh
+++ P-pacman.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:saintdle/pacman-tanzu.git
+git clone --depth 1 git@github.com:saintdle/pacman-tanzu.git
 cd pacman-tanzu/
 bash ./pacman-install.sh
 cd ..
