a8d7b2b6cb8d338cb0c4c9983b7ac704f54ca462
Masahiro Takahashi <masezou@gmail.com>
2021-10-17 07:45:08 +0000

    added octant

--- 2-tool.sh
+++ 2-tool.sh
@@ -107,6 +107,40 @@ rm -rf LICENSE kubens_v${KUBECTX}_linux_${ARCH}.tar.gz
 apt -y install fzf
 fi
 
+# Install octant
+if [ ! -f /usr/local/bin/octant ]; then
+   OCTANT=0.24.0
+   if [ ${ARCH} = amd64 ]; then
+      curl -OL https://github.com/vmware-tanzu/octant/releases/download/v${OCTANT}/octant_${OCTANT}_Linux-64bit.deb
+      dpkg -i octant_${OCTANT}_Linux-64bit.deb
+     rm dpkg -i octant_${OCTANT}_Linux-64bit.deb
+   fi
+
+   if [ ${ARCH} = armd64 ]; then
+      curl -OL https://github.com/vmware-tanzu/octant/releases/download/v${OCTANT}/octant_${OCTANT}_Linux-ARM.deb
+      dpkg -i octant_${OCTANT}_Linux-ARM.deb
+      rm octant_${OCTANT}_Linux-ARM.deb
+   fi
+   mkdir -p $HOME/.config/octant/plugins/
+git clone --depth 1 git@github.com:ashish-amarnath/octant-velero-plugin.git
+cd octant-velero-plugin/
+make install
+cd ..
+
+  mkdir -p $HOME/.config/octant/plugins/ && \
+  curl -L https://github.com/bloodorangeio/octant-helm/releases/download/v0.2.0/octant-helm_0.2.0_linux_amd64.tar.gz | \
+  tar xz -C ~/.config/octant/plugins/ octant-helm
+
+  mkdir -p $HOME/.config/octant/plugins/ 
+  git clone --depth 1 git@github.com:vmware-tanzu/octant-plugin-for-kind.git
+  cd octant-plugin-for-kind
+  make
+  cp bin/octant-plugin-for-kind $HOME/.config/octant/plugins/
+
+fi
+
+echo "OCTANT_LISTENER_ADDR=0.0.0.0:8090" >> /etc/profile
+
 # Misc
 apt -y install postgresql-contrib postgresql-client mysql-client jq lynx
 systemctl stop postgresql
