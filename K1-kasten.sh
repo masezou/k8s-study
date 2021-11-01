2ae5878a654aa8be383fb8b98fb526a035ed1950
Masahiro Takahashi <masezou@gmail.com>
2021-11-01 04:49:01 +0000

    bug fix2

--- K1-kasten.sh
+++ K1-kasten.sh
@@ -78,16 +78,14 @@ TOOLSVER=4.5.1
 rm -rf /usr/local/bin/k10tools
 if [ ! -f /usr/local/bin/k10tools ]; then
 curl -OL https://github.com/kastenhq/external-tools/releases/download/${TOOLSVER}/k10tools_${TOOLSVER}_linux_${ARCH}.tar.gz
-tar xfz k10tools_${TOOLSVER}_linux_${ARCH}.tar.gz
-mv k10tools_${TOOLSVER}_linux_${ARCH} /usr/local/bin/k10tools
+tar xfz k10tools_${TOOLSVER}_linux_${ARCH}.tar.gz -C /usr/local/bin
 chmod +x /usr/local/bin/k10tools
 fi
 
 rm -rf /usr/local/bin/k10multicluster
 if [ ! -f /usr/local/bin/k10multicluster ]; then
 curl -OL https://github.com/kastenhq/external-tools/releases/download/${TOOLSVER}/k10multicluster_${TOOLSVER}_linux_${ARCH}.tar.gz
-tar xfz k10multicluster_${TOOLSVER}_linux_${ARCH}.tar.gz
-mv k10multicluster_${TOOLSVER}_linux_${ARCH}  /usr/local/bin/k10multicluster
+tar xfz k10multicluster_${TOOLSVER}_linux_${ARCH}.tar.gz -C /usr/local/bin
 chmod +x /usr/local/bin/k10multicluster
 fi
 
